a025b25908c5cebb721b7114570740e6032e6bdf
quintonmills <quintonmills98@gmail.com>
2025-09-12 09:57:22 -0400

    refactor todo item to own conmponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("learn react"), new Todo("learn typescript")];
   return (
     <div>
-      <Todos />
+      <Todos items={todos} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,15 @@
-function Todos() {
+import React from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn Typescript</li>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
